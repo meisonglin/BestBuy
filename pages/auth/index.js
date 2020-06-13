6da63e9e08e9263b464ac2c2b651bcf9eeb60a40
itmsl <1096558208@qq.com>
2020-06-13 23:19:09 +0800

    小程序完结

---
+++ pages/auth/index.js
@@ -0,0 +1,37 @@
+import { request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
+import { login } from "../../utils/asyncWx.js";
+
+Page({
+    // 获取用户信息
+    async handleGetUserInfo(e) {
+        try {
+
+            // 1 获取用户信息
+            const { encryptedData, rawData, iv, signature } = e.detail;
+            // 2 获取小程序登录成功后的code
+            const { code } = await login();
+            const loginParams = { encryptedData, rawData, iv, signature, code };
+
+            wx.showToast({
+                title: '暂未开通此功能',
+                icon: 'none',
+                image: '',
+                duration: 1500,
+                mask: true,
+            });
+
+            /*     
+                //  3 发送请求 获取用户的token
+                const {token}=await request({url:"/users/wxlogin",data:loginParams,method:"post"});
+                // 4 把token存入缓存中 同时跳转回上一个页面
+                wx.setStorageSync("token", token);
+                wx.navigateBack({
+                  delta: 1
+                });
+               */
+        } catch (error) {
+            console.log(error);
+        }
+    }
+})
